90c86e248b8f7e82e7cb8bc7d24d6bd1cd71af14
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2023-04-27 13:56:02 -0300

    Creating component Loading for waited the loading of anything

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
-import { ActivityIndicator, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 
 import { ThemeProvider } from 'styled-components/native';
 
 import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans';
 
 import theme from '@theme/index';
+
 import { Home } from '@screens/Home';
+
+import { Loading } from '@components/Loading';
 
 export default function App() {
 
@@ -20,7 +23,8 @@ export default function App() {
       translucent
     />
 
-    {fontsLoaded ? <Home /> : <ActivityIndicator />}
+    {fontsLoaded ? <Home /> : <Loading />}
+
     </ThemeProvider>
   );
 }
