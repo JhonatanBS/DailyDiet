76663ae95aa2d2db3a07867e599ac79e1acf457a
Jhonatan <jhonatanb.s4nt0s@gmail.com>
2023-04-27 11:27:41 -0300

    Creating type, theme and inserting theme with styled-components

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
-import { Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+
+import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans';
+
+import { Text } from 'react-native';
+
+import theme from '@theme/index';
 
 export default function App() {
   return (
-    <View>
+    <ThemeProvider theme={theme}>
       <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    </ThemeProvider>
   );
 }
